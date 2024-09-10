23f795b9c817644abfde21a23c8a1234df9d8607
Karl Gharios <karlgharios@hotmail.com>
2024-09-10 23:09:59 +0300

    test

--- runOnDemand.sh
+++ runOnDemand.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+#!/bin/sh
+
+
 curl -H "Authorization: Bearer $SUPERVISOR_TOKEN" \
         -H "Content-Type: application/json" \
         -d '{"entity_id": "button.front_door_start_p2p_stream"}' \
         http://supervisor/core/api/services/button/press
 
-ffmpeg -loglevel debug -rtsp_transport tcp   \
-     -i rtsp://karlgharios:tonymontana@192.168.0.148:8554/front_door \
-     -c:v copy -f rtsp rtsp://localhost:8559/proxy_xxx_front_door
+ffmpeg -rtsp_transport tcp   \
+     -i rtsp://$1@192.168.0.148:8554/T8B00511233623B2 \
+     -c:v copy -preset veryfast  -f rtsp rtsp://localhost:8554/proxy_xxx_front_door
